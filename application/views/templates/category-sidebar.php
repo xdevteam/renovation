30173b008ed992168574e286d5b02d97764c60f2
DV <emeraldmihail@gmail.com>
2015-08-23 19:54:25 +0300

    edit footer and categories

--- application/views/templates/category-sidebar.php
+++ application/views/templates/category-sidebar.php
@@ -1,12 +1,13 @@
 <div class="col-sm-2 col-md-2 col-lg-2 col-xs-3 tabs-category-container">
                         <section class="widget-product-categories">
-                            <h3 class="widget-title">Категории</h3>
+                            <h4 class="widget-title"><img src="http://cms.loc/images/dom.png">Каталог продукции</h4>
                             <ul class="product-cat">                        
                                 <?php                     
                                 foreach ($cat_list as $cat => $subcategory) {
                                     foreach ($subcategory as $k => $v) {
                                         ?>                        
                                         <li class="cat-item cat-parent" data-ajax="<?= $k ?>">
+                                            <img src="http://cms.loc/images/strl.png">
                                             <a href="<?= base_url(); ?>subcategories/<?= $k ?>"><?= $cat ?></a>
                                             <span class="count hidden-xs">(<?= count($v) ?>)</span>
                                             <ul class="children">      
